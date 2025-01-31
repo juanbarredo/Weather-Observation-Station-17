0ea7578c842a0c734615dec9779213af4d15c377
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-01-30 20:08:09 -0500

    01 30 2025
    Just setting up the template

---
+++ Solution Weather Observation Station 17.sql
@@ -0,0 +1,9 @@
+-------------------------------01 30 2025-------------------------------------------
+--Style guide UPDATE
+
+--USE database
+--SELECT field_name
+--	FROM table_name
+--		WHERE condition.  --this is the newest UPDATE.
+
+--Also!
